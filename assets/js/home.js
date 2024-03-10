1d73e0e4b0f169c2073372f031a8b0f771fa897b
Leonardo Galindez <88455461+Leonardo-Galindez@users.noreply.github.com>
2024-03-10 06:17:51 -0300

    home terminado

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,15 +2,19 @@ const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
 
 var section_slider = document.querySelector(".section-slider");
+var footer = document.querySelector(".footer");
+
 navToggle.addEventListener("click", () => {
   navMenu.classList.toggle("nav-menu_visible");
 
   if (navMenu.classList.contains("nav-menu_visible")) {
     navToggle.setAttribute("aria-label", "Cerrar menú");
     section_slider.style.display = "none";
+    footer.style.display = "none";
   } else {
     navToggle.setAttribute("aria-label", "Abrir menú");
     section_slider.style.display = "block";
+    footer.style.display = "block";
   }
 });
 
--- home.php
+++ home.php
@@ -23,7 +23,9 @@ if (!isset($_SESSION['usuario'])) {
     <title>Computex</title>
     <link rel="icon" href="assets/images/favicon.ico">
     <link rel="stylesheet" href="assets/css/home.css" />
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
@@ -92,12 +94,28 @@ if (!isset($_SESSION['usuario'])) {
         </div>
     </section>
 
+    <footer class="footer">
+        <div class="contenedor-redes">
+            <div class="footer-link">
+                <div class="link">
+                    <h3>Siguenos</h3>
+                    <div class="socials">
+                        <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fa-brands fa-google-plus-g"></i></a>
+                        <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
     <script type="text/javascript">
         var counter = 1;
-        setInterval(function(){
+        setInterval(function () {
             document.getElementById('radio' + counter).checked = true;
             counter++;
-            if(counter > 3){
+            if (counter > 3) {
                 counter = 1;
             }
         }, 5000);
@@ -105,4 +123,5 @@ if (!isset($_SESSION['usuario'])) {
 
     <script src="assets/js/home.js"></script>
 </body>
+
 </html>
